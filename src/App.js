79f22f1ac97eccb42933e556c3e15c0bfc26f2f9
Naveen Kumar <54549550+navku0014@users.noreply.github.com>
2024-12-14 23:39:00 +0530

    front-end work over

--- src/App.js
+++ src/App.js
@@ -1,14 +1,5 @@
 import './App.css';
-import Navigation from './customer/components/navigation/Navigation';
-import HomePage from './customer/pages/homePage/HomePage';
-import Footer from './customer/components/footer/Footer';
-import Product from './customer/components/Product/Product';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import ProductDetails from './customer/components/productDetails/ProductDetails';
-import Cart from './customer/components/cart/Cart';
-import Checkout from './customer/components/checkOut/CheckOut';
-import Order from './customer/components/order/Order';
-import OrderDetails from './customer/components/order/OrderDetails';
 import CustomerRouters from './routers/CustomerRouters';
 
 function App() {
